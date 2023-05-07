b44a1c8aeefaf030d144acfc5a3c806cab8f73d7
Lautaro Garcia <lautaroegarcia1995@gmail.com>
2023-05-07 20:16:09 -0300

    mostrar resultado

--- Ejercicio_3.c
+++ Ejercicio_3.c
@@ -7,14 +7,13 @@ int main () {
     /* Escribir un programa en lenguaje C que lea una temperatura en grados Celsius como valor decimal y
        obtenga la temperatura Fahrenheit equivalente */
 
+    float GC;
 
-    double GC, GF;
-
-    printf ("\n\nIngrese una temperatura en grados celcius: ");
-    fflush(stdin);;
+    printf ("\nIngrese una temperatura en grados celcius: ");
+    fflush(stdin);
     scanf("%f", &GC);
 
-
+    printf("\n\nLa temperatura ingresada en grados celcius es de: %g lo que es igual a %g grados fahrenheit\n\n", GC, (1.8)*GC+32);
 
 
 
