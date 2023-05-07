a70d612dbe435a1832fd7a0fdaa9f493bdd144cd
Lautaro Garcia <lautaroegarcia1995@gmail.com>
2023-05-07 19:03:46 -0300

    Ejercicio 2
    Consigna del ejercicio 2 de la guia 1: introduccion

---
+++ Ejercicio_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    //Consigna
+    /*Escribir un programa para convertir una medida dada en pies a sus equivalentes en: Yardas, Pulgadas, Centímetros, Metros.
+    1 pie = 12 pulgadas, 1 yarda = 3 pies, 1 pulgada = 2,54 Cm Leer el número de pies e imprimir el número de yardas, pulgadas, centímetros y metros.  */
+
+
+
+
+}
