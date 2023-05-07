3531cd86a125c0466e33dbaa27efcebb273fcf62
Lautaro Garcia <lautaroegarcia1995@gmail.com>
2023-05-07 19:10:59 -0300

    declaracion de variables

--- Ejercicio_2.c
+++ Ejercicio_2.c
@@ -7,6 +7,8 @@ int main () {
     /*Escribir un programa para convertir una medida dada en pies a sus equivalentes en: Yardas, Pulgadas, Centímetros, Metros.
     1 pie = 12 pulgadas, 1 yarda = 3 pies, 1 pulgada = 2,54 Cm Leer el número de pies e imprimir el número de yardas, pulgadas, centímetros y metros.  */
 
+    int pies, yardas, pulgadas, centimetros, metros;
+
 
 
 
