f4ff8e1cb1d254617aa296ab08347bad80183090
Lautaro Garcia <lautaroegarcia1995@gmail.com>
2023-05-07 18:58:01 -0300

    conversion
    convertirmos e ingresamos resultados en las variables

--- Ejercicio_1.c
+++ Ejercicio_1.c
@@ -7,12 +7,17 @@ int main () {
     /*Escribir un programa que convierta un número dado en segundos en su equivalente
       en Horas, Minutos y Segundos. Ejemplo: Segundos=4000, el programa muestra 1H: 6M: 40S. */
 
+    int numero, horas, minutos, segundos;
 
-    int segundos;
-
-    printf("\nPor favor ingrese un numero entero para pasar a horas: ");
+    printf("\nPor favor ingrese un numero para pasar a horas: ");
     fflush(stdin);
-    scanf("%d", &segundos);
+    scanf("%d", &numero);
+
+
+    horas = numero / 3600;
+    minutos = ((numero - horas * 3600)/60);
+    segundos = numero % 60;
+
 
 
 
