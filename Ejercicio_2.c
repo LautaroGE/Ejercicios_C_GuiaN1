72edd1179fc58b9dd3b461dd9ba031ce831d9857
Lautaro Garcia <lautaroegarcia1995@gmail.com>
2023-05-07 19:18:37 -0300

    cambios y verificacion
    arreglo en las variables y verificacion de conversion a yardas

--- Ejercicio_2.c
+++ Ejercicio_2.c
@@ -7,12 +7,14 @@ int main () {
     /*Escribir un programa para convertir una medida dada en pies a sus equivalentes en: Yardas, Pulgadas, Centímetros, Metros.
     1 pie = 12 pulgadas, 1 yarda = 3 pies, 1 pulgada = 2,54 Cm Leer el número de pies e imprimir el número de yardas, pulgadas, centímetros y metros.  */
 
-    int pies, yardas, pulgadas, centimetros, metros;
+    int pies;
+    double yardas, pulgadas, centimetros, metros;
 
     printf ("Ingrese una medida dada en pies: ");
     fflush(stdin);
     scanf ("%d", &pies);
 
-
+    yardas = pies*0.33;
+    printf ("%g", yardas);
 
 }
