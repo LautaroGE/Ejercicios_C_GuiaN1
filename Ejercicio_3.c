bc08b5fc9ebc6cabbeea3c1429b858bc9693db6e
Lautaro Garcia <lautaroegarcia1995@gmail.com>
2023-05-07 19:54:05 -0300

    Ejercicio 3
    Consigna e ingreso de datos del ejercicio numero tres de la guia numero 1: introduccion
    
    Escribir un programa en lenguaje C que lea una temperatura en grados Celsius como valor decimal y obtenga la temperatura Fahrenheit equivalente

---
+++ Ejercicio_3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    //Consigna.
+    /* Escribir un programa en lenguaje C que lea una temperatura en grados Celsius como valor decimal y
+       obtenga la temperatura Fahrenheit equivalente */
+
+
+    double GC, GF;
+
+    printf ("\n\nIngrese una temperatura en grados celcius: ");
+    fflush(stdin);;
+    scanf("%f", &GC);
+
+
+
+
+
+}
