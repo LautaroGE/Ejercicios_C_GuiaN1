540ce881e3bc3c1675f7eda60555298bd80b07cc
Lautaro Garcia <lautaroegarcia1995@gmail.com>
2023-05-07 18:17:41 -0300

    Ejercicio_1
    Ejercicio numero 1 de la guia Introducción.
    
    Escribir un programa que convierta un número dado en segundos en su equivalente en Horas, Minutos y Segundos. Ejemplo: Segundos=4000, el programa muestra 1H: 6M: 40S.

---
+++ Ejercicio_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    //Consigna
+    /*Escribir un programa que convierta un número dado en segundos en su equivalente
+      en Horas, Minutos y Segundos. Ejemplo: Segundos=4000, el programa muestra 1H: 6M: 40S. */
+
+
+
+
+
+
+}
