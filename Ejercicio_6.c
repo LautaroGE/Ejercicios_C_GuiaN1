5960346f100e3d560f1c35a74fa295ac792485a2
Lautaro Garcia <lautaroegarcia1995@gmail.com>
2023-05-07 22:32:21 -0300

    Ingreso de datos

---
+++ Ejercicio_6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    //Consigna
+    //Escribir un programa que lea dos enteros de tres dígitos e imprima su producto con el siguiente formato:
+    /* 325
+       X426
+       1950
+       650
+      1300_
+      138450
+    */
+
+    int num1, num2;
+
+    printf("\nIngrese dos numeros de tres digitos: ");
+    fflush(stdin);
+    scanf("%d %d", &num1, &num2);
+
+
+
+
+    system ("pause");
+}
