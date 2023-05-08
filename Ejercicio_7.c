51e61a731d1fc63cc7cb895ce004934361468880
Lautaro Garcia <lautaroegarcia1995@gmail.com>
2023-05-08 02:27:27 -0300

    Ejercicio 7 e ingreso de datos

---
+++ Ejercicio_7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    //Consigna
+    //Construir un programa que al recibir como dato un número de 4 dígitos, genere una impresión del numero uno a uno
+
+    int numero;
+
+    //ingresamos los datos
+    printf("\nIngrese un numero de 4 digitos: ");
+    fflush(stdin);
+    scanf("%d", &numero);
+
+
+
+
+    system ("pause");
+
+
+}
