19fedb33c96fe61c6037a0d8e8f8c75e906a1894
Lautaro Garcia <lautaroegarcia1995@gmail.com>
2023-05-08 02:45:40 -0300

    ejercicio 8 e ingreso de datos

---
+++ Ejercicio_8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    //Consigna
+    //Escribir un programa que lea dos enteros en las variables X e Y, y a continuación obtenga los valores de: X / Y, y X % Y.
+    //Ejecute el programa varias veces con diferentes pares de enteros como entrada.
+
+    int x, y;
+
+    printf ("Ingrese los valores de x e y: ");
+    fflush(stdin);
+    scanf("%d %d", &x, &y);
+
+
+
+
+    system ("pause");
+}
