124f434457f874802b6b8eb2f7d9dc93d3627f98
Lautaro Garcia <lautaroegarcia1995@gmail.com>
2023-05-08 03:00:46 -0300

    completamos el ejercicio

--- Ejercicio_9.c
+++ Ejercicio_9.c
@@ -1,1 +1,21 @@
-#include
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    //Consigna
+    /*Escribir un programa que genere una impresión como la que se muestra a continuación:
+                XXXX
+               XX
+             XXX
+            XXX
+             XXX
+               XX
+                XXXX
+    */
+
+
+    printf ("     XXXX\n    XX\n  XXX\n XXX\n  XXX\n    XX\n     XXXX");
+
+
+}
