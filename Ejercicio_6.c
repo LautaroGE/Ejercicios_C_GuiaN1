c21d47c8cc2345b8131907259acb2d5bbaa82026
Lautaro Garcia <lautaroegarcia1995@gmail.com>
2023-05-08 01:56:32 -0300

    seguimos realizando y confirmando las operaciones

--- Ejercicio_6.c
+++ Ejercicio_6.c
@@ -22,10 +22,16 @@ int main () {
     int res1, res2, res3, total;
 
     //asignamos el ultimo digito de la variable num1 a res1
-    //para esto usamos el %, que tomará el ultimo digito y lo almacenara en la variable
-    res1 = num1 % 10;
-    //me aseguro que se haya almacenado correctamente
-    printf ("%d", res1);
+    //para esto usamos el %, que tomará el ultimo digito y lo almacenara en la variable. Multiplicamos el ultimo digito por num1
+    res1 = (num1 % 10) * num1;
+
+    //asignamos el anteultimo digito de la variable
+    //dividimos num1 x 10, para que mueva la coma. Como es un numero entero, el programa no tendra en cuenta el numero despues de la coma
+    //por ultimo, multiplicamos el digito x num1
+    res2 = (num1 / 10) % 10 * num1;
+
+    //nos aseguramos de que funcione correctamente
+    printf("%d", res2);
 
 
 
