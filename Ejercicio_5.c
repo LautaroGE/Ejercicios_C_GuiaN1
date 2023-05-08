f1a658097649eb2c4ad8b30dea14acf4f47809b8
Lautaro Garcia <lautaroegarcia1995@gmail.com>
2023-05-07 21:38:58 -0300

    ingreso de datos y corrección de printf

--- Ejercicio_5.c
+++ Ejercicio_5.c
@@ -6,6 +6,13 @@ int main () {
     //Consigna
     //Modificar el programa anterior de modo que lea una palabra de 5 letras y se imprima en el centro del rectángulo
 
+    char palabra[5];
+
+    printf ("\nIngrese una palabra de cinco letras: ");
+    fflush(stdin);
+    scanf("%s", &palabra);
+
+
     /* *********************
        *                   *
        *                   *
@@ -13,7 +20,7 @@ int main () {
        ********************* */
 
 
-       printf ("\n  ********************\n  *                  *\n  *                  *\n  *                  *\n  ********************");
+       printf ("\n  ********************\n  *                  *\n  *      %s       *\n  *                  *\n  ********************", palabra);
 
 
 
