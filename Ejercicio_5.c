5c58376e2fdc0c88cea5d9bebf746c03a81a07e0
Lautaro Garcia <lautaroegarcia1995@gmail.com>
2023-05-07 21:29:06 -0300

    Ejercicio 5 consigna
    Modificar el programa anterior de modo que lea una palabra de 5 letras y se imprima en el centro del rectángulo

---
+++ Ejercicio_5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    //Consigna
+    //Modificar el programa anterior de modo que lea una palabra de 5 letras y se imprima en el centro del rectángulo
+
+    /* *********************
+       *                   *
+       *                   *
+       *                   *
+       ********************* */
+
+
+       printf ("\n  ********************\n  *                  *\n  *                  *\n  *                  *\n  ********************");
+
+
+
+
+}
+
